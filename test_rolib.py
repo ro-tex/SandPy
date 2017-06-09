ddf782913dfc8de1374b8f10d4b974d135e02139
Ivaylo Novakov <inovakov@gmail.com>
2017-06-09 18:45:19 +0200

    Tests work now. Execute with py.test.

--- test_rolib.py
+++ test_rolib.py
@@ -7,6 +7,12 @@ class TestRoLib(unittest.TestCase):
     '''Based on https://docs.python.org/2/library/unittest.html
     '''
 
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
     def test_hello(self):
         self.assertEqual(rolib.hello(), 'Hello from RoLib!')
 
